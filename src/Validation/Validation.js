2a65a7511d0efd23ac9d24bf98c8c680392adcc3
Joe Scott <jbs198585@gmail.com>
2021-03-23 09:49:05 -0400

    Task number four complete

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 
 const validation = ( props ) => {
+  let validationMessage = 'Text long enough';
+
+  if (props.inputLength <= 5) {
+    validationMessage = 'Text too short';
+  }
+
   return (
-    <div>  
-      <p>Text too short!</p>
+    <div>
+      <p>{validationMessage}</p>
     </div>
   );
 };
